e32ce28c05b13e0614d415bbfe15718e127dc317
scori1 <freetiktik@gmail.com>
2022-02-16 07:12:37 +0700

    GiiXyz

---
+++ herman.js
@@ -0,0 +1,235 @@
+const
+	{
+		WAConnection,
+		MessageType,
+		Presence,
+		MessageOptions,
+		Mimetype,
+		WALocationMessage,
+		WA_MESSAGE_STUB_TYPES,
+		WA_DEFAULT_EPHEMERAL,
+		ReconnectMode,
+		ProxyAgent,
+		GroupSettingChange,
+		waChatKey,
+		mentionedJid,
+		processTime,
+	} = require("@adiwajshing/baileys")
+const fs = require("fs")
+const axios = require('axios')
+const speed = require("performance-now")
+const util = require('util')
+const crypto = require('crypto')
+const request = require('request')
+const { exec, spawn } = require('child_process')
+const fetch = require('node-fetch')
+const moment = require('moment-timezone')
+const ffmpeg = require('fluent-ffmpeg')
+const { removeBackgroundFromImageFile } = require('remove.bg')
+
+// Lib!
+
+const { fetchJosn, fetchText } = require('./lib/fetcher')
+const { color, bgcolor } = require('./lib/color')
+const { wait, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, close } = require('./lib/functions')
+const setting = JSON.parse(fs.readFileSync('./setting.json'))
+
+// Setting!
+
+autoread = setting.autoread
+autocomposing = setting.autocomposing
+autorecording = setting.autorecording
+
+// Owner!
+
+owner = setting.OwnerNumber
+botname = setting.BotName
+ownername = setting.OwnerName
+fake = setting.Fake
+
+// DataBase!
+
+const _antilink = JSON.parse(fs.readFileSync('./database/antilink.json'))
+
+// Module!
+		
+module.exports = herman = async (herman, mek, _welkom) => {
+	try {
+        if (!mek.hasNewMessage) return
+        mek = mek.messages.all()[0]
+		if (!mek.message) return
+		if (mek.key && mek.key.remoteJid == 'status@broadcast') return
+		global.blocked
+    	mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+        const content = JSON.stringify(mek.message)
+		const from = mek.key.remoteJid
+		const { text, extendedText, contact, contactsArray, groupInviteMessage, listMessage, buttonsMessage, location, liveLocation, image, video, sticker, document, audio, product, quotedMsg } = MessageType
+		const tanggal = moment.tz('Asia/Jakarta').format('dddd') + ', ' + moment.tz('Asia/Jakarta').format('LL')
+		const time = moment().tz('Asia/Jakarta').format("HH:mm:ss")
+		const timeMak = moment().tz('Asia/Makassar').format("HH:mm:ss");
+        const timeJay = moment().tz('Asia/Jayapura').format("HH:mm:ss");
+        const type = Object.keys(mek.message)[0]        
+        const cmd = (type === 'conversation' && mek.message.conversation) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text : ''.slice(1).trim().split(/ +/).shift().toLowerCase()
+        const prefix = /^[°•π÷×¶∆£¢€¥®™=|~!#$%^&.?/\\©^z+*@,;]/.test(cmd) ? cmd.match(/^[°•π÷×¶∆£¢€¥®™=|~!#$%^&.?/\\©^z+*,;]/gi) : '#'          	
+        body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message[type].caption.startsWith(prefix) ? mek.message[type].caption : (type == 'videoMessage') && mek.message[type].caption.startsWith(prefix) ? mek.message[type].caption : (type == 'extendedTextMessage') && mek.message[type].text.startsWith(prefix) ? mek.message[type].text : (type == 'listResponseMessage') && mek.message[type].singleSelectReply.selectedRowId ? mek.message[type].singleSelectReply.selectedRowId : (type == 'buttonsResponseMessage') && mek.message[type].selectedButtonId ? mek.message[type].selectedButtonId : ''
+		budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
+		const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()		
+		const args = body.trim().split(/ +/).slice(1)
+		const arg = budy.slice(command.length + 2, budy.length)
+		const c = args.join(' ')
+		const isCmd = body.startsWith(prefix)
+		const q = args.join(' ')
+		const Verived = "0@s.whatsapp.net"
+		const txt = mek.message.conversation
+		const botNumber = herman.user.jid
+		const ownerNumber = [`${owner}@s.whatsapp.net`, `6281333603591@s.whatsapp.net`]
+		const isGroup = from.endsWith('@g.us')
+		let sender = isGroup ? mek.participant : mek.key.remoteJid
+		let senderr = mek.key.fromMe ? herman.user.jid : mek.key.remoteJid.endsWith('@g.us') ? mek.participant : mek.key.remoteJid
+		const groupMetadata = isGroup ? await herman.groupMetadata(from) : ''.toString
+		const groupName = isGroup ? groupMetadata.subject : ''
+		const groupId = isGroup ? groupMetadata.jid : ''
+		const groupMembers = isGroup ? groupMetadata.participants : ''
+		const groupDesc = isGroup ? groupMetadata.desc : ''
+		const groupOwner = isGroup ? groupMetadata.owner : ''
+		const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+		const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
+		const isGroupAdmins = groupAdmins.includes(sender) || false
+        const senderNumber = sender.split("@")[0]
+        const hour_now = moment().format('HH:mm:ss')
+		const conts = mek.key.fromMe ? herman.user.jid : herman.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+        const pushname = mek.key.fromMe ? herman.user.name : conts.notify || conts.vname || conts.name || '-'    
+    
+		const isAntiLink = isGroup ? _antilink.includes(from) : false
+		const isWelkom = isGroup ? _welkom.includes(from) : false
+		const isOwner = ownerNumber.includes(sender)
+		const isMybot = isOwner || mek.key.fromMe
+		let bio_nya = await herman.getStatus(sender)
+		try {
+			bio_user = `${bio_nya.status}`
+		} catch {
+			bio_user = '-'
+			}
+
+// Button!
+
+const sendButLocation = async (id, text1, desc1, gam1, but = [], options = {}) => {
+kma = gam1
+mhan = await herman.prepareMessage(from, kma, location)
+const buttonMessages = {
+locationMessage: mhan.message.locationMessage,
+contentText: text1,
+footerText: desc1,
+buttons: but,
+headerType: 6
+}
+herman.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
+}
+  
+// Conection1
+
+        const sendMediaURL = async(to, url, text="", mids=[]) =>{
+                if(mids.length > 0){
+                    text = normalizeMention(to, text, mids)
+                }
+                const fn = Date.now() / 10000;
+                const filename = fn.toString()
+                let mime = ""
+                var download = function (uri, filename, callback) {
+                    request.head(uri, function (err, res, body) {
+                        mime = res.headers['content-type']
+                        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                    });
+                };
+                download(url, filename, async function () {
+                    console.log('done');
+                    let media = fs.readFileSync(filename)
+                    let type = mime.split("/")[0]+"Message"
+                    if(mime === "image/gif"){
+                        type = MessageType.video
+                        mime = Mimetype.gif
+                    }
+                    if(mime.split("/")[0] === "audio"){
+                        mime = Mimetype.mp4Audio
+                    }
+                    herman.sendMessage(to, media, type, { quoted: ftrol, mimetype: mime, caption: text,contextInfo: {"mentionedJid": mids}})
+                    
+                    fs.unlinkSync(filename)
+                });
+            }  
+
+// Connection2
+
+const hideTag = async function(from, text){
+           let anu = await herman.groupMetadata(from)
+           let members = anu.participants
+           let ane = []
+           for (let i of members){
+           ane.push(i.jid)
+}
+           herman.sendMessage(from, {text:text, jpegThumbnail:fs.readFileSync('./media/herman.jpg')}, 'extendedTextMessage', {contextInfo: {"mentionedJid": ane}})
+}
+
+switch (command) {
+
+// Menunya!
+
+case 'kick':
+if (!isGroup) return (only.group)
+if (!isGroupAdmins && !mek.key.fromMe) return (only.admin)
+if (!isBotGroupAdmins) return (only.Badmin)
+if (
+mek.message.extendedTextMessage === undefined ||
+mek.message.extendedTextMessage === null
+)
+return ("*Tag Target 🎯*");
+mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid;
+if (mentioned.length > 1) {
+herman.groupRemove(from, mentioned);
+(success);
+} else if (mentioned.length < 1) {
+anu = mek.message.extendedTextMessage.contextInfo.participant;
+herman.groupRemove(from, [anu]);
+(mess.success);
+} else {
+herman.groupRemove(from, mentioned);
+(success);
+}
+break
+
+// Akhir!
+				
+default:
+if (isOwner) {
+if (budy.startsWith('$')){
+if (!mek.key.fromMe && !isOwner) return
+qur = budy.slice(2)
+exec(qur, (err, stdout) => {
+if (err) return reply(`${err}`)
+if (stdout) {
+reply(stdout)
+}
+})
+}
+if (isOwner) {
+if (budy.startsWith('>')) {
+console.log(color('[ EVAL ]'), color(moment(mek.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`eval return`))
+try {
+let evaled = await eval(budy.slice(2))
+if (typeof evaled !== 'string') evaled = require('util').inspect(evaled)
+reply(`${evaled}`)
+} catch (err) {
+reply(`${err}`)
+}
+}
+}
+}
+}
+	} catch (e) {
+    e = String(e)
+    if (!e.includes("this.isZero") && !e.includes("jid")) {
+	console.log('Error : %s', color(e, 'red'))
+        }
+	// console.log(e)
+	}
+}
